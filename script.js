445ef5abd7d5b29fc752a31a5b1bd31bb2c27656
supersuper261 <148677532+supersuper261@users.noreply.github.com>
2024-01-31 21:47:55 +0800

    Update script.js

--- script.js
+++ script.js
@@ -1,7 +1,5 @@
-function toggleLetterCase() {
-    document.querySelector('.letter-case').classList.toggle('open');
-}
-
 function toggleEnvelope(envelope) {
     envelope.classList.toggle("open");
+    // Add or remove 'letter-case-open' class to toggle the opening of the letter case
+    document.querySelector('.letter-case').classList.toggle('letter-case-open');
 }
